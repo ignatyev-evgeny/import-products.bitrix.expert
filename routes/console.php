33a7388beb19826ae35a8e2a76c6ec783e6fb437
Evgeny Ignatyev <ignatyev.ew@gmail.com>
2024-10-15 13:58:56 +0300

    Настройка Horizon

--- routes/console.php
+++ routes/console.php
@@ -9,3 +9,4 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote')->hourly();
 
 Schedule::command(updateAccessTokenCommand::class)->everyFifteenMinutes();
+Schedule::command('horizon:snapshot')->everyFiveMinutes();

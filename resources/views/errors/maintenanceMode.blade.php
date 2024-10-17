5075f426dc883b14ccc6def9dae80d81cd78991b
Evgeny Ignatyev <ignatyev.ew@gmail.com>
2024-10-17 16:54:17 +0300

    Добавлен функционал включения / отключения приложения.

---
+++ resources/views/errors/maintenanceMode.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>404</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://getbootstrap.com/docs/4.0/examples/sign-in/signin.css" rel="stylesheet">
+</head>
+<body class="text-center">
+<form class="form-signin" style="max-width: 500px;">
+    <img class="mb-4" src="{{ asset('img/maintenance.png') }}" alt="" width="144" height="144">
+    <h1 class="h3 mb-3 font-weight-normal">Мы обновляем наше приложение. <br>Совсем скоро вернемся ☺️</h1>
+    <p class="mt-5 mb-3 text-muted">&copy; 2024</p>
+</form>
+</body>
+</html>

ed40a91e2070beb87ae0d407bb59f5f3ed843ad4
Evgeny Ignatyev <ignatyev.ew@gmail.com>
2024-10-17 22:09:40 +0300

    Обновление базовых методов импорта и экспорта товарных позиций

--- app/Http/Controllers/SyncController.php
+++ app/Http/Controllers/SyncController.php
@@ -13,10 +13,14 @@ use Maatwebsite\Excel\Facades\Excel;
 class SyncController extends Controller {
 
     protected Bitrix24Service $bitrixService;
+    private mixed $domain;
 
-    public function __construct(Bitrix24Service $bitrixService)
+    public function __construct(Request $request)
     {
-        $this->bitrixService = $bitrixService;
+        parse_str(parse_url($request->header('referer'), PHP_URL_QUERY), $queryParams);
+        $domain = $queryParams['DOMAIN'];
+        $this->domain = $domain;
+        $this->bitrixService = new Bitrix24Service($domain);
     }
 
     public function importProcess(Request $request)
@@ -28,56 +32,57 @@ class SyncController extends Controller {
         try {
             $this->bitrixService->getSmartProcessDetail();
             $filePath = storage_path('app/' . $request->file('file')->store('temp'));
-            ProcessImportJob::dispatch($filePath, $this->bitrixService);
-
-            $this->bitrixService->sendNotify($this->bitrixService->getAssigned(), 'Файл добавлен в очередь на обработку.');
-            return response()->json(['message' => 'Файл добавлен в очередь на обработку.']);
+            ProcessImportJob::dispatch(
+                $filePath,
+                $this->bitrixService
+            );
+            $this->bitrixService->sendNotify(
+                $this->bitrixService->getAssigned(),
+                'Файл добавлен в очередь на обработку.'
+            );
+            return response()->json([
+                'message' => 'Файл добавлен в очередь на обработку.'
+            ]);
         } catch (Exception $exception) {
-            return response()->json(['message' => $exception->getMessage()], 500);
+            return response()->json([
+                'message' => $exception->getMessage()
+            ], 500);
         }
     }
 
     public function exportProcess()
     {
         try {
-
             $smartProcessDetail = $this->bitrixService->getSmartProcessDetail();
             $getProductRows = $this->bitrixService->getProductRows($smartProcessDetail['SYMBOL_CODE_SHORT']);
-
             $exportData = [];
             $count = 1;
-            $bitrixService = new Bitrix24Service();
-            $domain = $bitrixService->getDomain();
-            $integration = Integration::where('domain', $domain)->first();
-
+            $integration = Integration::where('domain', $this->domain)->first();
             foreach ($getProductRows as $getProductRow) {
-                $getProductRow['detail'] = $bitrixService->productDetail($getProductRow['productId']);
-
-                if(empty($getProductRow['detail'])) {
-                    abort(500, 'Ошибка при получении детальной информации по товарной позиции. Попробуйте еще раз или свяжитесь с технической поддержкой.');
+                $getProductRow['detail'] = $this->bitrixService->productDetail($getProductRow['productId']);
+                if (!empty($getProductRow['detail'])) {
+                    throw new Exception('Ошибка при получении детальной информации по товарной позиции. <br> Попробуйте еще раз или свяжитесь с технической поддержкой.');
                 }
-
                 $exportData[] = [
                     $count++,
-                    $getProductRow['detail']['PROPERTY_'.$integration->product_field_article]['value'] ?? '',
-                    $getProductRow['detail']['PROPERTY_'.$integration->product_field_brand]['value'] ?? '',
+                    $getProductRow['detail']['PROPERTY_' . $integration->product_field_article]['value'] ?? '',
+                    $getProductRow['detail']['PROPERTY_' . $integration->product_field_brand]['value'] ?? '',
                     $getProductRow['productName'],
                     $getProductRow['price'],
                     $getProductRow['quantity'],
                 ];
             }
-
-            return Excel::download(new ExportProductRows($exportData), 'exported_data.xlsx');
+            $fileName = 'exported_data_' . time() . '.xlsx';
+            Excel::store(new ExportProductRows($exportData), 'public/' . $fileName);
+            $downloadUrl = asset('storage/' . $fileName);
+            return response()->json([
+                'download_url' => $downloadUrl
+            ]);
         } catch (Exception $exception) {
             report($exception);
-            return response()->json(['message' => $exception->getMessage()], 500);
+            return response()->json([
+                'message' => $exception->getMessage()
+            ], 500);
         }
     }
-
-
-
-
-
-
-
 }
